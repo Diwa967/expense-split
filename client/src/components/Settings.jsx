57fb6611bb99a4bb080865e72f20041dce6cf86b
Diwa967 <khatiwadadiwakar96@gmail.com>
2026-05-16 00:33:20 +0545

    update

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import Sidebar from "../components/Layout/Sidebar";
+import Sidebar from "../components/layout/Sidebar";
 import {
   FaUser,
   FaCamera,
